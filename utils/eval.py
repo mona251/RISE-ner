f25e8133921a5d323614505ea9f52fc52c6e35ae
mona251 <96126623+mona251@users.noreply.github.com>
2023-12-05 20:26:51 +0100

    Update eval.py

--- utils/eval.py
+++ utils/eval.py
@@ -24,19 +24,21 @@ def compute_metrics(p, metric, label_list):
         "accuracy": results["overall_accuracy"],
     }
 
+def compute_inference_metrics(label, pred, metric, confusion_matrix, label_list, label2id):
+    
+    true_predictions = [label_list[p] for (p, l) in zip(pred, label) if l != -100]
+    true_labels = [label_list[l] for (p, l) in zip(pred, label) if l != -100]
 
-def compute_inference_metrics(label, pred, metric, label_list):
-
-    true_predictions = [[label_list[p] for (p, l) in zip(pred, label) if l != -100]]
-    true_labels = [[label_list[l] for (p, l) in zip(pred, label) if l != -100]]
-
-    results = metric.compute(predictions=true_predictions, references=true_labels)
+    confusion_pred = [label2id[i] for i in true_predictions]
+    confusion_label = [label2id[i] for i in true_labels]
+    results = metric.compute(predictions=[true_predictions], references=[true_labels])
+    confusion = confusion_matrix.compute(predictions=confusion_pred, references=confusion_label)
     return {
         "precision": results["overall_precision"],
         "recall": results["overall_recall"],
         "f1": results["overall_f1"],
         "accuracy": results["overall_accuracy"],
-    }
+    }, confusion
 
 def compute_scores(tokenized_datasets, model, metric, label_list, device):
     scores = []
