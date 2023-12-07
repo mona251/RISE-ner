b640fd783b03d5ac1467acfcc7c48a0a6d3b85ff
mona251 <96126623+mona251@users.noreply.github.com>
2023-12-07 21:33:10 +0100

    Update eval.py

--- utils/eval.py
+++ utils/eval.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from tqdm import tqdm
+from sklearn.metrics import cohen_kappa_score
 
 
 def compute_metrics(p, metric, label_list):
@@ -33,9 +34,10 @@ def compute_inference_metrics(label, pred, metric, confusion_matrix, label_list,
     confusion_label = [label2id[i] for i in true_labels]
     results = metric.compute(predictions=[true_predictions], references=[true_labels])
     confusion = confusion_matrix.compute(predictions=confusion_pred, references=confusion_label)
+    kappa = cohen_kappa_score(true_predictions, true_labels)
     return {
         "precision": results["overall_precision"],
         "recall": results["overall_recall"],
         "f1": results["overall_f1"],
         "accuracy": results["overall_accuracy"],
-    }, confusion, results
+    }, confusion, results, kappa
